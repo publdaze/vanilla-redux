8462d926876f76db94231bdca21b4e62c2f1d521
kej <publdaze@naver.com>
2022-01-30 10:48:25 +0900

    3.5 Detail Screen

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,23 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+const Detail = ({ toDo }) => {
+  const params = useParams();
+  //console.log(params.id);
+  //console.log(toDo);
+  const toDoText = toDo.find((toDo) => toDo.id === parseInt(params.id));
+  console.log(toDoText);
+  return (
+    <>
+      {toDoText.text}
+      Created at {params.id}
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { toDo: state };
+};
+
+export default connect(mapStateToProps)(Detail);
