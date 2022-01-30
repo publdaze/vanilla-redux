33b3265543c90fc36cf15a455917e40b0a0430b4
kej <publdaze@naver.com>
2022-01-30 11:54:49 +0900

    4.4 createSlice

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { actionCreators } from "../store";
+import { add } from "../store";
 import ToDo from "../components/ToDo";
 
 function Home({ toDos, addToDo }) {
@@ -36,8 +36,7 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   // 이걸 통해 이제 props 바꿀 수 있음
   return {
-    // addToDo라는 function은 text argument가 필요하고, 이 함수가 실행되면 actionCreators.addRoDo(text)와 함께 dispatch를 호출한다.
-    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+    addToDo: (text) => dispatch(add(text)),
   };
 }
 
