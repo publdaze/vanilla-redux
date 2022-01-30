d490b32ad2fa1db1ec8959b275f96aaa57ed42d0
kej <publdaze@naver.com>
2022-01-30 10:06:13 +0900

    3.3 mapDispatchToProps 실습

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -26,4 +28,12 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  // 이걸 통해 이제 props 바꿀 수 있음
+  return {
+    // addToDo라는 function은 text argument가 필요하고, 이 함수가 실행되면 actionCreators.addRoDo(text)와 함께 dispatch를 호출한다.
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
