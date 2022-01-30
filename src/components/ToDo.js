bff97e390251cfa82ac99283c015d211e9890dd1
kej <publdaze@naver.com>
2022-01-30 11:04:12 +0900

    4.1 createAction 실습

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,9 +6,8 @@ import { Link } from "react-router-dom";
 function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>DELETE</button>
-      </Link>
+      <Link to={`/${id}`}>{text} </Link>
+      <button onClick={onBtnClick}>DELETE</button>
     </li>
   );
 }
